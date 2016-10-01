0b55226b0495860189bee19ba8e2f943f0e3b888
f-fong <feliciasfong@gmail.com>
2016-09-30 17:35:55 -0700

    Implement App and SearchYouTube code to pass test specs

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search?key=' + options.key + '&q=' + options.query + '&maxResults=' + options.max + '&part=snippet&videoEmbeddable=true&type=video',
+    success: (data) => {
+      callback(data.items);
+    },
+    error: (exception) => {
+      console.error('Error', exception);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
